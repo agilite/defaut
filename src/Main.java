4785b5e6e7afa20928a469b6476a45b7ef8a9db9
KC07 <cyrille.kirscher@etudiant.univ-lille1.fr>
2015-09-03 13:59:14 +0200

    edit Main

--- src/Main.java
+++ src/Main.java
@@ -9,45 +9,42 @@ public class Main {
 		String repUtilisateur;
 		
 		System.out.println("Bienvenue dans Town of Glory \nNom de la ville:");
-		repUtilisateur= sc.nextLine();
-		
+		repUtilisateur=sc.nextLine();
 		
 		Ville jeu = new Ville(repUtilisateur);
 		System.out.println(jeu);
-		System.out.println("\nPour construire appuyer sur c \n" +
-						   "Pour passez la journée appuyez sur j \n" +
-						   "Pour supprimer appuyez sur s");
-		repUtilisateur= sc.nextLine();
-		if(repUtilisateur.equals("c")){
-			System.out.println("Pour construire une maison appuyez sur m \n" +
-							   "Pour construire une mine appuyez sur o \n" +
-							   "Pour construire un entrepot appuyez sur e \n" +
-							   "Pour construire une eglise appuyez sur g \n" +
-							   "Pour construire un champs appuyez sur p \n" +
-							   "Pour construire une scierie appuyez sur s");
+		
+		while(!jeu.isOver()) {
+			System.out.println("\nPour construire, appuyez sur c\n" +
+					   		   "Pour passer la journée, appuyez sur j\n" +
+					   		   "Pour supprimer, appuyez sur s");
 			repUtilisateur= sc.nextLine();
-			if(repUtilisateur.equals("m")){
-				jeu.addBatiment(TypeBatiment.MAISON);
-				System.out.println(jeu);
-			}
-			if(repUtilisateur.equals("o")){
-				jeu.addBatiment(TypeBatiment.MINE);
-				System.out.println(jeu);
-			}
-			if(repUtilisateur.equals("e")){
-				jeu.addBatiment(TypeBatiment.ENTREPOT);
-				System.out.println(jeu);
-			}
-			if(repUtilisateur.equals("g")){
-				jeu.addBatiment(TypeBatiment.EGLISE);
-				System.out.println(jeu);
-			}
-			if(repUtilisateur.equals("p")){
-				jeu.addBatiment(TypeBatiment.CHAMPS);
-				System.out.println(jeu);
-			}
-			if(repUtilisateur.equals("s")){
-				jeu.addBatiment(TypeBatiment.SCIERIE);
+			if(repUtilisateur.equals("c")){
+				System.out.println("Pour construire une maison, appuyez sur m\n" +
+						   		   "Pour construire une mine, appuyez sur o\n" +
+						   		   "Pour construire un entrepot, appuyez sur e\n" +
+						   		   "Pour construire une eglise, appuyez sur g\n" +
+						   		   "Pour construire un champ, appuyez sur p\n" +
+								   "Pour construire une scierie, appuyez sur s");
+				repUtilisateur= sc.nextLine();
+				if(repUtilisateur.equals("m")){
+					jeu.addBatiment(TypeBatiment.MAISON);
+				}
+				if(repUtilisateur.equals("o")){
+					jeu.addBatiment(TypeBatiment.MINE);
+				}
+				if(repUtilisateur.equals("e")){
+					jeu.addBatiment(TypeBatiment.ENTREPOT);
+				}
+				if(repUtilisateur.equals("g")){
+					jeu.addBatiment(TypeBatiment.EGLISE);
+				}
+				if(repUtilisateur.equals("p")){
+					jeu.addBatiment(TypeBatiment.CHAMPS);
+				}
+				if(repUtilisateur.equals("s")){
+					jeu.addBatiment(TypeBatiment.SCIERIE);
+				}
 				System.out.println(jeu);
 			}
 		}
